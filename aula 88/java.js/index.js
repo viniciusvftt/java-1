9cb0e70abb000f74c91baea21ee24866dfdb6097
Vinicius Felipe Tenorio <100444356+viniciusvftt@users.noreply.github.com>
2022-04-18 21:42:25 -0400

    Add files via upload

---
+++ aula 88/java.js/index.js
@@ -0,0 +1,22 @@
+$.support.cors = true;
+
+
+
+$('#btpagina2').click(
+    function (){
+        $('#conteudo').load("Pages/pagina02.html")
+    }
+)
+
+
+
+
+
+
+
+$('#btpagina1').click(
+    function (){
+       $('#conteudo').load("pages/pagina01.html")
+    }
+)
+
